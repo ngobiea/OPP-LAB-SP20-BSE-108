b76f5381a9787ad203285f8b0ad7444ba6143817
ngobiea <70290199+ngobiea@users.noreply.github.com>
2021-04-27 21:09:04 +0500

    Update Lab09_Clock_Runner.java

--- Lab09/Task1/Lab09_Clock_Runner.java
+++ Lab09/Task1/Lab09_Clock_Runner.java
@@ -1,6 +1,6 @@
 package Lab09.Task01;
 
-public class Lab08_Clock_Runner {
+public class Lab09_Clock_Runner {
     public static void main(String[] args) {
 
         ClockExtended c1 = new ClockExtended(20,54,23);
